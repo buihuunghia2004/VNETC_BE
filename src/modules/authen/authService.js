24d7acbead6506a23b0d087ec97e008a9a1d0ab9
Nguyễn Quý Đôn <anctaone0511@gmail.com>
2024-08-24 10:34:26 +0700

    .

--- src/modules/authen/authService.js
+++ src/modules/authen/authService.js
@@ -1,28 +1,37 @@
-import {StatusCodes} from "http-status-codes"
+import {StatusCodes} from "http-status-codes";
+
 import {env} from "~/config/environment"
 import {jwtHelper} from "~/helper/jwtHelper"
 import {Account} from "~/models/accountModel"
 import ApiErr from "~/utils/ApiError"
 import bcrypt from 'bcryptjs'
 
+const jwt = require('jsonwebtoken');
+
 const login = async (email, password) => {
     try {
-        const account = await Account.findOne({email});
+        const account = await Account.findOne({ email });
         if (!account) {
-            throw new ApiErr(StatusCodes.NOT_FOUND, 'Account not found');
+            throw new ApiErr(StatusCodes.UNAUTHORIZED, 'Invalid credentials');
         }
 
-        const passwordMatch = bcrypt.compareSync(password, account.password);
+        const passwordMatch = await bcrypt.compare(password, account.password);
         if (!passwordMatch) {
-            throw new ApiErr(StatusCodes.UNAUTHORIZED, 'Wrong password');
+            throw new ApiErr(StatusCodes.UNAUTHORIZED, 'Invalid credentials');
         }
 
+        // Tạo JWT token
         const token = await jwtHelper.generateToken(account, env.ACCESS_TOKEN_SECRET, env.ACCESS_TOKEN_LIFE);
-        return token;
+        const decoded = jwt.decode(token); // Giải mã token để lấy thông tin payload
+
+        return { token: token, decoded: decoded };
     } catch (error) {
-        throw error;
+        console.error("Login error:", error);
+        throw new ApiErr(StatusCodes.INTERNAL_SERVER_ERROR, 'Login failed');
     }
 };
+
+
 const logout = async () => {
     try {
         return "Logout Success"
