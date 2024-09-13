77b69b97f0ec42eeea95b83af440842b24f28eab
Nguyễn Quý Đôn <anctaone0511@gmail.com>
2024-09-13 09:21:39 +0700

    limitSizeOptimized

--- src/utils/uploadImage.js
+++ src/utils/uploadImage.js
@@ -1,13 +1,17 @@
-import cloudinary from "~/helper/cloundinary"
+import cloudinary from "~/helper/cloundinary";
 
 const uploadImageToCloudinary = async (filePaths) => {
     try {
         const uploadPromises = filePaths.map((filePath) =>
-            cloudinary.uploader.upload(filePath.path)
+            cloudinary.uploader.upload(filePath.path, {
+                crop: "limit",
+                quality: "auto",
+                fetch_format: "auto"
+            })
         );
 
         const uploadResults = await Promise.all(uploadPromises);
-        console.log(uploadResults)
+        console.log(uploadResults);
         return uploadResults.map(result => result.secure_url);
     } catch (error) {
         throw new Error(`Image upload failed: ${error.message}`);
