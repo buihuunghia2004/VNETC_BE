433a588ecf232260e22b6aa0aab991f7a5dd8660
donbmt92 <anctaone0511@gmail.com>
2024-09-26 11:20:52 +0700

    .

--- src/utils/uploadSingleImage.js
+++ src/utils/uploadSingleImage.js
@@ -1,23 +1,23 @@
-import cloudinary from "~/helper/cloundinary";
+import cloudinary from "~/helper/cloundinary"
 
 const uploadSingleImageToCloudinary = (filePath) => {
-    return new Promise((resolve, reject) => {
-        cloudinary.uploader.upload(
-            filePath,
-            {
-                crop: "limit",
-                quality: "auto",
-                fetch_format: "auto"
-            },
-            (err, result) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(result);
-                }
-            }
-        );
-    });
-};
+  return new Promise((resolve, reject) => {
+    cloudinary.uploader.upload(
+      filePath,
+      {
+        crop: "limit",
+        quality: "auto",
+        fetch_format: "auto",
+      },
+      (err, result) => {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(result)
+        }
+      }
+    )
+  })
+}
 
-module.exports = uploadSingleImageToCloudinary;
+module.exports = uploadSingleImageToCloudinary
